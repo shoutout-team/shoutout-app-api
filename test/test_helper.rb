7f8d24204fce953edb6a27e77c4918d814c58ebd
Michael Ajwani <michael.ajwani@i22.de>
2020-03-14 20:35:45 +0100

    configured test-stack | Issue #1

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,42 @@
+#--------------------------------------------------------------------------------
+# NOTE: when changing migrations do not forget to run: rake db:test:prepare
+#--------------------------------------------------------------------------------
+
 ENV['RAILS_ENV'] ||= 'test'
+
+require 'simplecov'
+SimpleCov.start
+
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'minitest/rails'
+require 'minitest/pride'
+require 'minitest-optional_retry'
 
-class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+if RUBY_PLATFORM.match?(/darwin/)
+  require 'minitest/autorun'
+  require 'minitest/macos_notification'
+  require 'minitest/reporters'
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  Minitest::Reporters.use!(
+    [
+      Minitest::Reporters::SpecReporter.new,
+      Minitest::Reporters::MacosNotificationReporter.new(title: 'Shoppyr-Tests')
+    ],
+    ENV,
+    Minitest.backtrace_filter
+  )
+end
 
-  # Add more helper methods to be used by all tests here...
+module ActiveSupport
+  class TestCase
+    # Run tests in parallel with specified workers
+    # parallelize(workers: :number_of_processors)
+
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    # fixtures :all
+
+    # Add more helper methods to be used by all tests here...
+    include FactoryBot::Syntax::Methods
+  end
 end
