e9c7e9af774fa6e6a06246cf48bb00fbafb0a7c2
Michael Ajwani <michael.ajwani@i22.de>
2020-03-21 17:41:03 +0100

    added concern JsonRendering | Issue #4

---
+++ app/controllers/concerns/json_rendering.rb
@@ -0,0 +1,39 @@
+module JsonRendering
+  extend ActiveSupport::Concern
+
+  def render_json(result = {})
+    render json: result, status: :ok
+  end
+
+  alias render_json_success render_json
+
+  def render_json_unprocessable(result = {})
+    render json: result, status: :unprocessable_entity
+  end
+
+  def render_json_error(result = {})
+    render json: result, status: '500'
+  end
+
+  def render_json_forbidden(error_or_plain_message, additional_result = {})
+    response = error_or_plain_message
+    response = { error: response } unless response.is_a?(Hash)
+    render json: response.merge(Hash(additional_result)), status: :forbidden
+  end
+
+  # TODO: Checkout improvements from #123
+  # And we should rename key :html to :markup
+  def render_json_partial(partial_path, locals = {})
+    render json: { html: render_to_string(partial: partial_path, locals: locals) }, layout: false
+  end
+
+  def render_json_markup(markup)
+    render json: { html: markup }, layout: false
+  end
+
+  private def ensure_json_request
+    return if request.format == :json
+
+    render nothing: true, status: :not_acceptable
+  end
+end
