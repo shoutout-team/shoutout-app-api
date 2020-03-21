497c22bf397fbc64af60489ff29e33b6367cf052
Michael Ajwani <michael.ajwani@i22.de>
2020-03-21 19:48:08 +0100

    fixed seeds | Issue #11

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,5 @@ unless User.any?
   User.create(email: 'henry.steinhauer@i22.de', name: 'Henry', password: 'Development123!', role: :developer, approved: true)
   User.create(email: 'dominik.criado@i22.de', name: 'Dominik', password: 'Development123!', role: :administrator, approved: true)
   User.create(email: 'lena.overkamp@i22.de', name: 'Lena', password: 'Development123!', role: :administrator, approved: true)
-  User.create(email: 'jessica.willius.de', name: 'Jessi', password: 'Development123!', role: :administrator, approved: true)
+  User.create(email: 'jessica.willius@i22.de', name: 'Jessi', password: 'Development123!', role: :administrator, approved: true)
 end
