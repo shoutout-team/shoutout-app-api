8123df0909e4d99805647ff36020f7d23f14775c
Michael Ajwani <michael.ajwani@i22.de>
2020-03-21 17:12:41 +0100

    coded basic app-config | Issue #2

---
+++ app/models/app/config.rb
@@ -0,0 +1,21 @@
+module App
+  class Config
+    def self.fetch
+      Rails.application.config_for(:app)
+    end
+
+    TITLE = fetch[:title]
+
+    DOMAIN = fetch[:domains][:live]
+    PRODUCTION_DOMAIN = fetch[:domains][:production]
+    STAGING_DOMAIN = fetch[:domains][:staging]
+    PREVIEW_DOMAIN = fetch[:domains][:preview]
+
+    PRODUCTION_BACKEND = 'https://' + PRODUCTION_DOMAIN + '/backend'.freeze
+    STAGING_BACKEND = 'https://' + STAGING_DOMAIN + '/backend'.freeze
+    PREVIEW_BACKEND = 'https://' + PREVIEW_DOMAIN + '/backend'.freeze
+    DEVELOPMENT_BACKEND = '/backend'.freeze
+
+    DEBUG_MODE_APP = fetch[:debug][:app]
+  end
+end
