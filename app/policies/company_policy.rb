cc3762d2754e1c67dc778af282754e59ebc52543
Michael Ajwani <michael.ajwani@i22.de>
2020-03-31 18:52:59 +0200

    added missing policy | Issue #75

---
+++ app/policies/company_policy.rb
@@ -0,0 +1,15 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    record.gid == user
+  end
+
+  def update?
+    record.gid == user
+  end
+end
